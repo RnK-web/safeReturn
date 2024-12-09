6d9b3c03820bca644f5e18c1cc7dd08433ee3d7c
RnK-web <yann.daugrois@gmail.com>
2024-12-09 17:44:00 +0100

    Basic Spring boot app + H2 in memory database config

---
+++ src/main/java/fr/uge/api/safeReturn/SafeReturnApplication.java
@@ -0,0 +1,13 @@
+package fr.uge.api.safeReturn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeReturnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeReturnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/uge/api/safeReturn/SafeReturnApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uge.api.safeReturn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeReturnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
